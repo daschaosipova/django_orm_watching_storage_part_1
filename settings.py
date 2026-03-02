6e5c2d3b91b6ec22403e08d8c5a5bc8fb0c824b6
daschaosipova <daschaosipova@yandex.com>
2026-03-02 05:25:33 +0300

    Update DB variables in .env and settings

--- settings.py
+++ settings.py
@@ -4,12 +4,12 @@ load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('ENGINE'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
     }
 }
 
